fdffe1320cd51119b0d8a790e810ed74535ecebb
WebWithRathor <devrajrathor143@gmail.com>
2024-06-30 22:41:15 +0530

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import GridSection from './GridSection'
+import Nav from './Nav'
 
 const Layout = () => {
   return (
     <div className='h-full'>
-        <Banner/>
-        <GridSection/>
+      <Nav />
+      <Banner />
+      <GridSection />
     </div>
   )
 }
