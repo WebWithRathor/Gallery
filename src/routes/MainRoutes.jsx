22a15a6346cff9c710c7a4e6297207e56f696a04
WebWithRathor <devrajrathor143@gmail.com>
2024-06-30 22:41:42 +0530

    search functionality added

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout'
+import Search from '../components/Search'
 
 const MainRoutes = () => {
   return (
     <>
         <Routes>
             <Route path='/' element={<Layout/>}/>
+            <Route path='/search/:query' element={<Search/>}/>
         </Routes>
     </>
   )
