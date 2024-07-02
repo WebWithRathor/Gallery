892dcb5af561767ecaa287f5f2343404a3fad5bf
WebWithRathor <devrajrathor143@gmail.com>
2024-07-02 22:01:34 +0530

    .

--- src/components/partials/SingleImage.jsx
+++ src/components/partials/SingleImage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const SingleImage = ({ backgroundColor, src }) => {
+const SingleImage = ({ backgroundColor, src, photographer}) => {
     return (
-        <div  style={{ backgroundColor}} className=" bg-blue-50 h-[70vh] overflow-hidden">
-        <img src={src} className='h-full w-full object-cover' alt="" />
+        <div style={{ backgroundColor }} className="rounded group relative bg-blue-50 h-[70vh] overflow-hidden">
+            <div className="overlay bg-black/[.4] flex-col justify-end cursor-pointer text-white p-6 group-hover:flex hidden absolute h-full w-full">
+                <h1 className=' text-lg'>{photographer}</h1>
+            </div>
+            <img src={src} className='h-full w-full object-cover' alt="" />
         </div>
     )
 }
